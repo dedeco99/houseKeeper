452fb3c441d821148bd645ab6b1f6cba98569a6a
dedeco99 <dedeco99@gmail.com>
2021-01-21 20:32:56 +0000

    Added list type widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int level = 0;
+  List<String> groceries = ["Leite", "Cereais", "Bolachas", "Tremoços"];
 
   @override
   Widget build(BuildContext context) {
@@ -22,97 +22,25 @@ class _AppState extends State<App> {
         title: Text("House Keeper"),
         elevation: 0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            level++;
-          });
-        },
-        child: Icon(Icons.add),
-      ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/avatar.png"),
-                radius: 40,
-              ),
-            ),
-            Divider(
-              height: 60,
-              color: Colors.grey[400],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'NAME',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        letterSpacing: 2,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Rodrigo',
-                      style: TextStyle(
-                        color: Colors.amberAccent[200],
-                        letterSpacing: 2,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      'AUTISM LEVEL',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        letterSpacing: 2,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "$level",
-                      style: TextStyle(
-                        color: Colors.amberAccent[200],
-                        letterSpacing: 2,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 10),
-                Text(
-                  "rodrigo@rabbitsoftware.dev",
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: groceries.map((grocery) {
+            return Center(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  grocery,
                   style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18,
-                    letterSpacing: 1,
+                    fontSize: 28,
+                    letterSpacing: 2,
+                    color: Colors.grey[200],
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
