e6a771befc8ab676935f5f6126a831b8e79d689e
dedeco99 <dedeco99@gmail.com>
2021-01-21 20:21:58 +0000

    Added a more advanced layout

--- lib/main.dart
+++ lib/main.dart
@@ -6,28 +6,114 @@ void main() {
   ));
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int level = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text("House Keeper"),
-      ),
-      body: Center(
-        child: Text(
-          "gotem",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2,
-            color: Colors.white,
-          ),
-        ),
+        elevation: 0,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text("click"),
+        onPressed: () {
+          setState(() {
+            level++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/avatar.png"),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[400],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'NAME',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Rodrigo',
+                      style: TextStyle(
+                        color: Colors.amberAccent[200],
+                        letterSpacing: 2,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'AUTISM LEVEL',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "$level",
+                      style: TextStyle(
+                        color: Colors.amberAccent[200],
+                        letterSpacing: 2,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10),
+                Text(
+                  "rodrigo@rabbitsoftware.dev",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
