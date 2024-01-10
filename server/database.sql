4d78b880c8d7ca069ecb08a102d60cd625ee780d
dedeco99 <dedeco99@gmail.com>
2024-01-10 15:51:41 +0000

    Fixed created field in schemas

--- server/database.sql
+++ server/database.sql
@@ -5,7 +5,7 @@ CREATE TABLE category(
   id uuid PRIMARY KEY NOT NULL,
   active boolean NOT NULL DEFAULT TRUE,
   name text NOT NULL,
-  created timestamp
+  created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE grocery(
@@ -15,14 +15,14 @@ CREATE TABLE grocery(
   category uuid REFERENCES category(id),
   default_quantity smallint NOT NULL DEFAULT 1,
   default_price numeric NOT NULL DEFAULT 0,
-  created timestamp
+  created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE grocery_list(
   id uuid PRIMARY KEY NOT NULL,
   active boolean NOT NULL DEFAULT TRUE,
   name text NOT NULL,
-  created timestamp
+  created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE grocery_list_grocery(
@@ -31,7 +31,7 @@ CREATE TABLE grocery_list_grocery(
   grocery uuid REFERENCES grocery(id) NOT NULL,
   quantity smallint NOT NULL DEFAULT 1,
   price money NOT NULL DEFAULT 0,
-  created timestamp
+  created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Users
@@ -41,7 +41,7 @@ CREATE TABLE profile(
   name text NOT NULL,
   email text NOT NULL,
   household uuid REFERENCES household(id),
-  created timestamp,
+  created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   UNIQUE (email)
 );
 
@@ -49,6 +49,6 @@ CREATE TABLE household(
   id uuid PRIMARY KEY NOT NULL,
   active boolean NOT NULL DEFAULT TRUE,
   name text NOT NULL,
-  created timestamp
+  created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
