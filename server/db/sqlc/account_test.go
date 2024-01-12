cc4d2eea5b5e06ba9e5e15b5c94a03b2141d6c13
dedeco99 <dedeco99@gmail.com>
2024-01-12 15:31:26 +0000

    Added unit tests

---
+++ server/db/sqlc/account_test.go
@@ -0,0 +1,98 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/dedeco99/housekeeper/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomAccount(t *testing.T) Account {
+	arg := CreateAccountParams{Name: util.RandomString(6), Email: util.RandomString(6) + "@gmail.com"}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Name, account.Name)
+	require.Equal(t, arg.Email, account.Email)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.Created)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccounts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+
+	arg := GetAccountsParams{Limit: 5, Offset: 5}
+
+	accounts, err := testQueries.GetAccounts(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Name, account2.Name)
+	require.Equal(t, account1.Email, account2.Email)
+
+	require.WithinDuration(t, account1.Created, account2.Created, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	arg := UpdateAccountParams{ID: account1.ID, Name: util.RandomString(6), Email: util.RandomString(6) + "@gmail.com"}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, arg.Name, account2.Name)
+	require.Equal(t, arg.Email, account2.Email)
+
+	// TODO: test modify timestamp
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	account2, err := testQueries.DeleteAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account2.Active, false)
+
+	account3, err := testQueries.DeleteAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account3)
+
+	require.Equal(t, account3.Active, true)
+}
--- server/go.mod
+++ server/go.mod
@@ -3,3 +3,12 @@ module github.com/dedeco99/housekeeper
 go 1.21.5
 
 require github.com/google/uuid v1.5.0
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
