1db559296cebdb8679c265f2b52d4d24486aa546
dedeco99 <dedeco99@gmail.com>
2024-01-22 10:00:37 +0000

    Added tap on card action to edit grocery

--- client/lib/components/grocery_list_grocery_card.dart
+++ client/lib/components/grocery_list_grocery_card.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import "package:get_it/get_it.dart";
 
+import "package:housekeeper/components/grocery_list_grocery_detail.dart";
+
 import "package:housekeeper/services/groceries.dart";
 import "package:housekeeper/services/grocery.dart";
 
@@ -18,60 +20,66 @@ class _GroceryCardState extends State<GroceryListGroceryCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(children: [
-              Text(
-                widget.groceryListGrocery.grocery.name,
-                style: TextStyle(fontSize: 28, letterSpacing: 2),
-              )
-            ]),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
+    return GestureDetector(
+      onTap: () => showModalBottomSheet(
+        context: context,
+        builder: (context) => GroceryListGroceryDetail(groceryListGrocery: widget.groceryListGrocery),
+      ),
+      child: Card(
+        margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(children: [
                 Text(
-                  "${widget.groceryListGrocery.price} €",
-                  style: TextStyle(fontSize: 20, letterSpacing: 2),
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.remove),
-                      onPressed: widget.groceryListGrocery.quantity > 1
-                          ? () {
-                              groceries.addGroceryListGrocery(
-                                widget.groceryListGrocery.id,
-                                widget.groceryListGrocery.quantity - 1,
-                                widget.groceryListGrocery.price.toString(),
-                              );
-                            }
-                          : null,
-                    ),
-                    Text(
-                      "x${widget.groceryListGrocery.quantity}",
-                      style: TextStyle(fontSize: 20, letterSpacing: 2),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.add),
-                      onPressed: () {
-                        groceries.addGroceryListGrocery(
-                          widget.groceryListGrocery.id,
-                          widget.groceryListGrocery.quantity + 1,
-                          widget.groceryListGrocery.price.toString(),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                  widget.groceryListGrocery.grocery.name,
+                  style: TextStyle(fontSize: 28, letterSpacing: 2),
+                )
+              ]),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    "${widget.groceryListGrocery.price} €",
+                    style: TextStyle(fontSize: 20, letterSpacing: 2),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.remove),
+                        onPressed: widget.groceryListGrocery.quantity > 1
+                            ? () {
+                                groceries.addGroceryListGrocery(
+                                  widget.groceryListGrocery.id,
+                                  widget.groceryListGrocery.quantity - 1,
+                                  widget.groceryListGrocery.price.toString(),
+                                );
+                              }
+                            : null,
+                      ),
+                      Text(
+                        "x${widget.groceryListGrocery.quantity}",
+                        style: TextStyle(fontSize: 20, letterSpacing: 2),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.add),
+                        onPressed: () {
+                          groceries.addGroceryListGrocery(
+                            widget.groceryListGrocery.id,
+                            widget.groceryListGrocery.quantity + 1,
+                            widget.groceryListGrocery.price.toString(),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
