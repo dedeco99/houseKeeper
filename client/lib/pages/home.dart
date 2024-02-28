6216b588a8b9eb449ab70e556f8811ab4b734779
dedeco99 <dedeco99@gmail.com>
2024-02-28 11:19:08 +0000

    Added bottom navigation bar

--- client/lib/pages/home.dart
+++ client/lib/pages/home.dart
@@ -1,37 +1,34 @@
 import "package:flutter/material.dart";
-import "package:flutter_spinkit/flutter_spinkit.dart";
 
-class Home extends StatelessWidget {
+import "package:housekeeper/pages/groceries.dart";
+import "package:housekeeper/pages/grocery_list.dart";
+
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  final List<Widget> _pages = [const GroceryListView(), const GroceriesView()];
+
+  final List<NavigationDestination> _navigationOptions = [
+    const NavigationDestination(icon: Icon(Icons.local_grocery_store_rounded), label: "Grocery List"),
+    const NavigationDestination(icon: Icon(Icons.settings_rounded), label: "Settings"),
+  ];
+  int _selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Future.delayed(const Duration(seconds: 1)),
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.done:
-            return Scaffold(
-              appBar: AppBar(title: const Text("House Keeper")),
-              body: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton.icon(
-                      onPressed: () {
-                        Navigator.pushNamed(context, "/groceryList");
-                      },
-                      icon: const Icon(Icons.local_grocery_store),
-                      label: const Text("Grocery List"),
-                    )
-                  ],
-                ),
-              ),
-            );
-          default:
-            return const Center(child: SpinKitWave(color: Colors.white, size: 75));
-        }
-      },
+    return Scaffold(
+      body: Center(child: _pages.elementAt(_selectedIndex)),
+      bottomNavigationBar: NavigationBar(
+        destinations: _navigationOptions,
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (int index) => setState(() => _selectedIndex = index),
+        height: 65,
+      ),
     );
   }
 }
